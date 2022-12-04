6eb4d35c1dbf9e5f2d8581402aeb536d1abdd4df
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-12-04 18:02:27 +0800

    Add files via upload

--- dextensible.py
+++ dextensible.py
@@ -1,4 +1,4 @@
-import os, math
+import os, sys, platform, distro, math
 from typing import Union
 
 T = True
@@ -12,6 +12,24 @@ class _main():
     
     def getcwd():
         return os.getcwd()
+    
+    def argv(index: int):
+        return sys.argv[index]
+    
+    def getname():
+        return os.name
+    
+    def getos():
+        if platform.system() == 'Linux':
+            _name = distro.linux_distribution()
+            _version = distro.distro_release_info()
+        else:
+            _name = platform.system()
+            _version = platform.version()
+        return ' '.join([_name, _version])
+
+    def getpyver():
+        return platform.python_version()
 
     def include(self):
         __import__(self)
@@ -32,14 +50,21 @@ class _main():
     def extend(objs: list, tar: Union[tuple, list, dict, set]):
         tar.extend(objs)
 
+    def join(obj, sep: str):
+        return sep.join(obj)
+        
 
-
-    if __name__ != __file__:
-        _f = input(f'{os.getcwd()} @-> ')
-        #_f = 'text.dx' #test
-        if not _f.endswith('.dx'): raise FileExtensionError('Invalid file extension, file extension should be ".dx"')
-        with open(_f, 'r+') as _t:
-            for _i in _t:
-                if _i == '\n' or _i.startswith('//'): pass
-                else: print(_i); eval(_i)
+    # python dextensible.py test.dx
+    if __name__ == __file__:
+        if not sys.argv[2].endswith('.dx'): raise FileExtensionError('Invalid file extension, file extension should be ".dx"')
+        else:
+            with open(sys.argv[2], 'r+') as _t:
+                for _i in _t:
+                    if _i == '\n' or _i.startswith('//'): pass
+                    else: print(_i); eval(_i)
+    else: #testing session
+        with open('test.dx', 'r+') as _t:
+                for _i in _t:
+                    if _i == '\n' or _i.startswith('//'): pass
+                    else: print(_i); eval(_i)
 
